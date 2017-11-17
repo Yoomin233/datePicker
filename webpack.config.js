9353ef5f56f8d6eef3dba6d58e799f575197c027
Yuemin.Hu <hym920408@gmail.com>
2017-11-17 18:31:35 +0800

    adding commonChunk plugin

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
       'react-hot-loader/patch',
       './src/main.js'
     ],
+    main2: [
+      'react-hot-loader/patch',
+      './src/main2.js'
+    ],
   },
   cache: true,
   devtool: 'eval-source-map',
@@ -55,7 +59,19 @@ module.exports = {
       // favicon: __dirname + '/favicon.png',
       template: __dirname + '/src/index.html',
       filename: './index.html',
-      chunks: ['main']
+      chunks: ['main', 'main2', 'webpackRuntime', 'vendor']
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js',
+      // at least two chunks need to contain a specific module before it's moved in to the 'vendor(stated above)' chunk
+      minChunks: (module, count) => module.context && module.context.indexOf('node_modules') !== -1,
+      chunks: ['main', 'main2'],
+      minSize: 1024 * 1024
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'webpackRuntime',
+      minChunks: Infinity
+    })
   ]
 }
